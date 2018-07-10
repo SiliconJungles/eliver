cb23d4bdd4c46b8d33a2c9f7164d9ae5e60c49e4
Vincent <vinh.nglx@gmail.com>
2018-07-10 15:24:22 +0700

    Add the mix_version_regex to replace version after bump

--- lib/eliver/version_file.ex
+++ lib/eliver/version_file.ex
@@ -1,5 +1,6 @@
 defmodule Eliver.VersionFile do
   @version_regex ~r/([0-9]+\.[0-9]+\.[0-9]+)/
+  @mix_version_regex ~r/version: "([0-9]+\.[0-9]+\.[0-9]+)"/
 
   def version(filename \\ "VERSION") do
     case File.read(filename) do
@@ -15,10 +16,47 @@ defmodule Eliver.VersionFile do
     case File.read(filename) do
       {:ok, body} ->
         new_contents = Regex.replace(@version_regex, body, new_version)
+
+        mix_files()
+        |> Enum.each(fn x ->
+          rewrite_version(new_version, x)
+        end)
+
         File.write(filename, new_contents)
 
       {:error, _} ->
         nil
     end
+  end
+
+  defp rewrite_version(new_version, filename) do
+    case File.read(filename) do
+      {:ok, body} ->
+        new_contents = Regex.replace(@mix_version_regex, body, "version: \"#{new_version}\"")
+
+        File.write(filename, new_contents)
+
+      {:error, _} ->
+        nil
+    end
+  end
+
+  defp mix_files do
+    {mixes, _} =
+      System.cmd("find", [
+        ".",
+        "-type",
+        "f",
+        "-name",
+        "mix.exs",
+        "-not",
+        "-path",
+        "./deps/*",
+        "-not",
+        "-path",
+        "./*/**/node_modules/*"
+      ])
+
+    mixes |> String.split("\n", trim: true)
   end
 end
